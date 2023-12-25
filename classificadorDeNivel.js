1d9b9aae8a615b3295ad8bbb055caf4862906e05
André Aguiar <andre.aguiar522@gmail.com>
2023-12-25 17:47:16 -0300

    Comitando o código do desafio

--- classificadorDeNivel.js
+++ classificadorDeNivel.js
@@ -1,1 +1,37 @@
-console.log("olá mundo");
+let nomeDoHeroi = "Hiro"
+let nivelDoHeroi = 1050
+
+switch (true) {
+    case nivelDoHeroi <= 1000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ferro")
+        break
+
+    case nivelDoHeroi > 1001 && nivelDoHeroi <= 2000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Bronze")
+        break
+
+    case nivelDoHeroi > 2001 && nivelDoHeroi <= 5000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Prata")
+        break
+    
+    case nivelDoHeroi > 5001 && nivelDoHeroi <= 7000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ouro")
+        break
+        
+    case nivelDoHeroi > 7001 && nivelDoHeroi <= 8000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Platina")
+        break
+    
+    case nivelDoHeroi > 8001 && nivelDoHeroi <= 9000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível Ascendente")
+        break
+
+    case nivelDoHeroi > 9001 && nivelDoHeroi <= 10000:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível Imortal")
+        break
+
+    case nivelDoHeroi >= 10001:
+        console.log("O Herói de nome " + nomeDoHeroi + " está no nível Radiante")
+        break
+        
+}
